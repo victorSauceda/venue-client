f1148118ab37ea3d42d358f31b699a762b4db870
Victor <victorasauceda@gmail.com>
2020-02-13 16:52:38 -0800

    added Setting for stripe

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
@@ -8,6 +8,7 @@ import NotificationsIcon from "@material-ui/icons/Notifications";
 import PersonIcon from "@material-ui/icons/Person";
 import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
+import { Auth } from "aws-amplify";
 // import Link from "@material-ui/core/Link";
 import { Link } from "react-router-dom";
 const styles = theme => ({
@@ -25,49 +26,108 @@ const styles = theme => ({
 
 const Header = props => {
   const { classes, handleToggleDrawer } = props;
-  return (
-    <AppBar position="fixed">
-      <Toolbar disableGutters={true} classes={{ root: classes.toolbarRoot }}>
-        <IconButton
-          color="inherit"
-          aria-label="Open drawer"
-          onClick={handleToggleDrawer}
-          className={classes.menuButton}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography
-          variant="title"
-          color="inherit"
-          noWrap
-          className={classes.title}
-        >
-          <Link color="inherit" to="/">
-            Venue
-          </Link>
-        </Typography>
-        <Typography
-          variant="title"
-          color="inherit"
-          noWrap
-          className={classes.title}
-        >
-          {" "}
-          <Link color="inherit" to="/cart">
-            Cart
-          </Link>
-        </Typography>
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  useEffect(() => {
+    onLoad();
+  }, []);
 
-        <IconButton color="inherit">
-          <Badge badgeContent={4} color="secondary">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <IconButton color="inherit">
-          <PersonIcon />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    } catch (e) {
+      if (e !== "NO current user") {
+        alert(e);
+      }
+    }
+    setIsAuthenticating(false);
+  }
+  async function handleLogout() {
+    await Auth.signOUt();
+    userHasAuthenticated(false);
+    props.history.push("/login");
+  }
+  return (
+    !isAuthenticating && (
+      <AppBar position="fixed">
+        <Toolbar disableGutters={true} classes={{ root: classes.toolbarRoot }}>
+          <IconButton
+            color="inherit"
+            aria-label="Open drawer"
+            onClick={handleToggleDrawer}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="title"
+            color="inherit"
+            noWrap
+            className={classes.title}
+          >
+            <Link color="inherit" to="/">
+              Venue
+            </Link>
+          </Typography>
+          <Typography
+            variant="title"
+            color="inherit"
+            noWrap
+            className={classes.title}
+          >
+            {" "}
+            <Link color="inherit" to="/cart">
+              Cart
+            </Link>
+          </Typography>
+
+          <IconButton color="inherit">
+            <Badge badgeContent={4} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+          <IconButton color="inherit">
+            <PersonIcon />
+          </IconButton>
+          {isAuthenticated ? (
+            <>
+              <Typography
+                variant="title"
+                color="inherit"
+                noWrap
+                className={classes.title}
+              >
+                {" "}
+                <Link color="inherit" to="/settings">
+                  Settings
+                </Link>
+              </Typography>
+              <Typography
+                variant="title"
+                color="inherit"
+                noWrap
+                className={classes.title}
+                onClick={handleLogout}
+              >
+                Logout
+              </Typography>
+            </>
+          ) : (
+            <>
+              <Link color="inherit" to="/signup">
+                Sign Up
+              </Link>
+              <Link color="inherit" to="/login" appProps={userHasAuthenticated}>
+                Login
+              </Link>
+            </>
+          )}
+        </Toolbar>
+
+        {/* <Routes appProps ={{ isAuthenticated, userHasAuthenticated}} */}
+      </AppBar>
+    )
   );
 };
 
