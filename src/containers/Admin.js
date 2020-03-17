0085cd9dbb2fed50a7a0bdcfe5d9f74930dea1d5
Victor <victorasauceda@gmail.com>
2020-03-16 17:22:50 -0700

    pie chart

--- src/containers/Admin.js
+++ src/containers/Admin.js
@@ -2,6 +2,16 @@ import React from "react";
 import Card from "@material-ui/core/Card";
 import Typography from "@material-ui/core/Typography";
 import CardContent from "@material-ui/core/CardContent";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import items from "../data/transactions";
+import { VictoryPie } from "victory";
+import transactions from "../data/transactions";
 const classes = {
   icon: {
     // marginRight: theme.spacing(2)
@@ -21,7 +31,8 @@ const classes = {
     height: "100%",
     display: "flex",
     flexDirection: "column",
-    marginBottom: "20px !important"
+    marginBottom: "20px !important",
+    width: "35% !important "
   },
   cardMedia: {
     paddingTop: "56.25%" // 16:9
@@ -46,70 +57,161 @@ class Admin extends React.Component {
   }
   render() {
     console.log("this is a sanity check", this.props.cartItems);
+    let filteredArray = [];
+    transactions.map(item => {
+      item.itemsOrdered.forEach(element => {
+        filteredArray.push(element);
+      });
+    });
+
+    let veganCount = 0;
+    let ketoCount = 0;
+    let paleoCount = 0;
+    filteredArray.map(item => {
+      if (item.dietType == "vegan") {
+        veganCount++;
+      }
+      if (item.dietType == "keto") {
+        ketoCount++;
+      }
+      if (item.dietType == "paleo") {
+        paleoCount++;
+      }
+    });
+    console.log("vegan count:", veganCount);
+    console.log("keto count:", ketoCount);
+    console.log("paleo count:", paleoCount);
 
     return (
-      <Card style={{ marginBottom: "20px" }} className={classes.card}>
-        <h1
-          style={{
-            marginTop: "25px",
-            textAlign: "center",
-            fontFamily: "Rock Salt",
-            color: "blue",
+      <div>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Customer Name</TableCell>
 
-            textAlign: "center"
-          }}
+                <TableCell align="right">Restaruant Name</TableCell>
+                <TableCell align="right">Order_Id</TableCell>
+                <TableCell align="right">Items Ordered</TableCell>
+
+                <TableCell align="right">Price</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {items.map(row => (
+                <TableRow key={row.name}>
+                  <TableCell component="th" scope="row">
+                    {row.customerFirstName} {row.customerLastName}
+                  </TableCell>
+                  <TableCell align="right">{row.restaurantName}</TableCell>
+                  <TableCell align="right">{row.orderId}</TableCell>
+                  <TableCell align="right">
+                    {row.itemsOrdered[0].name}
+                  </TableCell>
+                  <TableCell align="right">{row.price}</TableCell>
+                </TableRow>
+              ))}{" "}
+              {/* <TableRow key="1">
+                <TableCell component="th" scope="row">
+                  John Doe
+                </TableCell>
+                <TableCell align="right">Keto's Diner</TableCell>
+                <TableCell align="right">123456</TableCell>
+                <TableCell align="right">
+                  keto chicken, fries, rice, ham, bannana, tomato, onion,
+                  cabbage, corn, olives, sausage, turkey, chorizo, feta,
+                  pineapple
+                </TableCell>
+                <TableCell align="right">4</TableCell>
+                <TableCell align="right">Keto</TableCell>
+                <TableCell align="right">$23</TableCell>
+              </TableRow> */}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <VictoryPie
+          data={[
+            { x: "Keto", y: ketoCount },
+            { x: "Paleo", y: paleoCount },
+            { x: "Vegan", y: veganCount }
+          ]}
+        />
+
+        <Card
+          style={{ marginTop: "5rem", marginBottom: "20px", width: "30%" }}
+          className={classes.card}
         >
-          what what
-        </h1>
-        <h3
-          style={{
-            marginBottom: "15px",
-            marginTop: "10px",
-            textAlign: "center"
-          }}
-        >
-          <em>{this.props.cartItems}</em>
+          <h1
+            style={{
+              marginTop: "25px",
+              textAlign: "center",
+              fontFamily: "Rock Salt",
+              color: "blue",
 
-          {/* <img src={item.dietTypeIcon} height="20" width="20" /> */}
-        </h3>
-        <p
-          style={{
-            textAlign: "right",
-            marginRight: "1rem",
-            marginBottom: ".4rem"
-          }}
-        >
-          {this.props.cartItems} miles{" "}
-        </p>
+              textAlign: "center"
+            }}
+          >
+            John Doe
+          </h1>
+          <h3
+            style={{
+              marginBottom: "15px",
+              marginTop: "10px",
+              textAlign: "center"
+            }}
+          >
+            <em>Customer_Id: 1234</em>
+            <br />
+            <em>Order_Id: 1234</em>
+            <br />
+            <em>Restaruant_Id: 4566</em>
 
-        <div
-          style={{
-            justifyContent: "space-evenly",
-            display: "flex",
-            marginTop: "20px"
-          }}
-        >
-          <Typography>
-            Price:<strong>${this.props.adder}</strong>
-          </Typography>
-        </div>
-        <CardContent className={classes.cardContent}>
-          <Typography
-            style={{ textAlign: "center" }}
-            gutterBottom
-            variant="h5"
-            component="h2"
-          ></Typography>
+            {/* <img src={item.dietTypeIcon} height="20" width="20" /> */}
+          </h3>
 
-          <Typography
-            style={{ textAlign: "center", height: "10rem" }}
-          ></Typography>
+          <div
+            style={{
+              marginTop: "4rem",
+              display: "center",
+              textAlign: "center"
+            }}
+          >
+            <Typography>
+              <h1>Order </h1>
+            </Typography>
 
-          <span> </span>
+            <Typography>
+              Item:<strong>Keto Chicken</strong>
+            </Typography>
+            <Typography>
+              Diet:<strong>Keto</strong>
+            </Typography>
 
-          {/* <Typography>Ingredients:{item.ingredients}</Typography> */}
-        </CardContent>
-      </Card>
+            <Typography>
+              Quantity:<strong>2</strong>
+            </Typography>
+          </div>
+
+          <CardContent className={classes.cardContent}>
+            <Typography
+              style={{ textAlign: "center", marginTop: "7rem" }}
+              gutterBottom
+              variant="h5"
+              component="h2"
+            >
+              Sub Total: $40
+            </Typography>
+
+            <Typography style={{ textAlign: "center", height: "10rem" }}>
+              <h1>Total: $45</h1>
+            </Typography>
+
+            <span> </span>
+
+            {/* <Typography>Ingredients:{item.ingredients}</Typography> */}
+          </CardContent>
+        </Card>
+      </div>
     );
   }
 }
