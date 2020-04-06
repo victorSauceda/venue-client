a9cc148df3d8bdd82fb74a81a10c924512c7a155
Victor <victorasauceda@gmail.com>
2020-04-06 10:26:51 -0700

    sandbox

--- src/containers/Sandbox.js
+++ src/containers/Sandbox.js
@@ -1,19 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { Elements, StripeProvider } from "react-stripe-elements";
-import Stripe from "./Stripe";
-import config from "../config";
+import React, { useState } from "react";
 
-function Sandbox(props) {
-  // const [isCardComplete, setIsCardComplete] = useState(false);
-  // const { appProps } = props;
-  return (
-    <>
-      <StripeProvider apiKey={"pk_test_YrkVfvkXfJctnYa9FTd6Y3P300w066reEG"}>
-        <Elements>
-          <Stripe {...props} />
-        </Elements>
-      </StripeProvider>
-    </>
-  );
+export default function Sandbox(props) {
+  console.log("props", props);
+  return <div>hello world</div>;
 }
-export default Sandbox;
