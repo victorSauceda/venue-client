c9be248faf76ff3558eb10c8039fb32aec608ebf
Victor <victorasauceda@gmail.com>
2020-04-14 11:05:12 -0700

    dev practice week

---
+++ src/containers/admi.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+
+const ComponentName = props => {
+  const [isClicked, setIsClicked] = useState(false);
+  return <>ComponentName</>;
+};
+
+export default ComponentName;
+const [isClicked, setIsClicked] = useState("");
+import React, { useState, useEffect } from "react";
+
+const ComponentName = props => {
+  return <>ComponentName</>;
+};
+
+export default ComponentName;
